2fda9cf6e9d2de4d53527e8fd39363d2b8f85a25
ppr350 <kobito610@gmail.com>
2024-07-07 16:27:06 +0100

    Working on graph

--- src/startup.js
+++ src/startup.js
@@ -17,13 +17,24 @@ const boardLayOut = [
 ]
 
 const boardAdjacencyList = {
-    '0': [                                      {'neighbour': '7', 'weight': 'down'}, {'neighbour': '1', 'weight': 'right'}],
-    '1': [{'neighbour': '0', 'weight': 'left'}, {'neighbour': '8', 'weight': 'down'}, {'neighbour': '2', 'weight': 'right'}],
-    '2': [{'neighbour': '1', 'weight': 'left'}, {'neighbour': '9', 'weight': 'down'}, {'neighbour': '3', 'weight': 'right'}],
-    '3': [{'neighbour': '2', 'weight': 'left'}, {'neighbour': '10', 'weight': 'down'}, {'neighbour': '4', 'weight': 'right'}],
-    '4': [{'neighbour': '3', 'weight': 'left'}, {'neighbour': '11', 'weight': 'down'}, {'neighbour': '5', 'weight': 'right'}],
-    '5': [{'neighbour': '4', 'weight': 'left'}, {'neighbour': '12', 'weight': 'down'}, {'neighbour': '6', 'weight': 'right'}],
-    '6': [{'neighbour': '5', 'weight': 'left'}, {'neighbour': '13', 'weight': 'down'},                                      ],
+    '0':  [{'neighbour': '7', 'weight': 'down'}, {'neighbour': '1', 'weight': 'right'}],
+    '1':  [{'neighbour': '0', 'weight': 'left'}, {'neighbour': '8', 'weight': 'down'},  {'neighbour': '2', 'weight': 'right'}],
+    '2':  [{'neighbour': '1', 'weight': 'left'}, {'neighbour': '9', 'weight': 'down'},  {'neighbour': '3', 'weight': 'right'}],
+    '3':  [{'neighbour': '2', 'weight': 'left'}, {'neighbour': '10', 'weight': 'down'}, {'neighbour': '4', 'weight': 'right'}],
+    '4':  [{'neighbour': '3', 'weight': 'left'}, {'neighbour': '11', 'weight': 'down'}, {'neighbour': '5', 'weight': 'right'}],
+    '5':  [{'neighbour': '4', 'weight': 'left'}, {'neighbour': '12', 'weight': 'down'}, {'neighbour': '6', 'weight': 'right'}],
+    '6':  [{'neighbour': '5', 'weight': 'left'}, {'neighbour': '13', 'weight': 'down'},                                      ],
+    '7':  [{'neighbour': '0', 'weight': 'up'},   {'neighbour': '8', 'weight': 'right'}, {'neighbour': '14', 'weight': 'down'}],
+    '8':  [{'neighbour': '7', 'weight': 'left'}, {'neighbour': '1', 'weight': 'top'},   {'neighbour': '9', 'weight': 'right'},  {'neighbour': '15', 'weight': 'down'}],
+    '9':  [{'neighbour': '8', 'weight': 'left'}, {'neighbour': '2', 'weight': 'top'},   {'neighbour': '10', 'weight': 'right'}, {'neighbour': '16', 'weight': 'down'}],
+    '10': [{'neighbour': '9', 'weight': 'left'}, {'neighbour': '3', 'weight': 'top'},  {'neighbour': '11', 'weight': 'right'}, {'neighbour': '17', 'weight': 'down'}],
+    '11': [{'neighbour': '10', 'weight': 'left'}, {'neighbour': '4', 'weight': 'top'},  {'neighbour': '12', 'weight': 'right'}, {'neighbour': '18', 'weight': 'down'}],
+    '12': [{'neighbour': '11', 'weight': 'left'}, {'neighbour': '5', 'weight': 'top'},  {'neighbour': '13', 'weight': 'right'}, {'neighbour': '19', 'weight': 'down'}],
+    '13': [{'neighbour': '12', 'weight': 'left'}, {'neighbour': '6', 'weight': 'top'},  {'neighbour': '14', 'weight': 'right'}, {'neighbour': '20', 'weight': 'down'}],
+    '14': [{'neighbour': '13', 'weight': 'left'}, {'neighbour': '7', 'weight': 'top'},  {'neighbour': '15', 'weight': 'right'}, {'neighbour': '21', 'weight': 'down'}],
+    '15': [{'neighbour': '14', 'weight': 'left'}, {'neighbour': '8', 'weight': 'top'},  {'neighbour': '16', 'weight': 'right'}, {'neighbour': '22', 'weight': 'down'}],
+    '16': [{'neighbour': '15', 'weight': 'left'}, {'neighbour': '9', 'weight': 'top'},  {'neighbour': '17', 'weight': 'right'}, {'neighbour': '23', 'weight': 'down'}],
+    '17':
 }
 
 // console.log(boardLayOut)
@@ -31,8 +42,11 @@ const boardAdjacencyList = {
 
 class legalMove {
     constructor(sourceVertex) {
-        
+        this.move = {
+            ['']
+        }
     }
+
 }
 
 
