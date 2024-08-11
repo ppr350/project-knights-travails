08de00b03c295982218645ea1ee8f3aac874b345
Chee Hong "Kobe" Wong <kobito610@gmail.com>
2024-08-11 10:01:29 +0100

    switch laptop

--- src/index.js
+++ src/index.js
@@ -1,105 +1,46 @@
 import _, { add, findLast } from "lodash";
 import "./style.css";
 
-// import { createBoard } from "./startup";
+console.log('Project Knights Travails, I can do it.')
 
-class knightBoard {
-    constructor() {
-        this.adjacentList = {}
+function Node(pos, path) {
+    if (pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) { // if position of the knight is out of range
+        return null                                             // do nothing
     }
-
-    addSquare = (x, y) => {
-        this.adjacentList[[x, y]] = []
-        return [x, y]
-    }
-
-    addEdge = (square1, square2) => {
-        this.adjacentList[square1].push(square2)
-    }
-
-    knightPositions = (x, y, n) => {
-        let square = this.addSquare(x, y)
-
-        if ((x+2 < n) && (y+1 < n)) {
-            this.addEdge(square, [x+2, y+1])
-        }
-        if ((x-2 >= 0) && (y+1 < n)) {
-            this.addEdge(square, [x-2, y+1])
-        }
-        if ((x+2 < n) && (y-1 >= 0)) {
-            this.addEdge(square, [x+2, y-1])
-        }
-        if ((x+1 < 0) && (y+1 < 0)) {
-            this.addEdge(square, [x-2, y-1])
-        }
-        if ((x+1 < n) && (y+2 < n)) {
-            this.addEdge(square, [x+1, y+2])
-        }
-        if ((x+1 < n) && (y-2 >= 0)) {
-            this.addEdge(square, [x+1, y-2])
-        }
-        if ((x-1 >= 0) && (y+2 < n)) {
-            this.addEdge(square, [x-1, y+2])
-        }
-        if ((x-1 >= 0) && (y-2 >= 0)) {
-            this.addEdge(square, [x-1, y-2])
-        }
-    }
-
-    allKnightPositions = (x, y, n) => {
-        if (!this.adjacentList[`${x},${y }`]) {
-            this.knightPositions(x, y, n)
-            this.adjacentList[`${x},${y}`].map(child => {
-                {
-                    this.allKnightPositions(child[0], child[1], n)
-                }
-            })
-        }
-    }
-
-    numberOfKnightMoves = (xStart, yStart, xEnd, yEnd, n) => {
-		this.allKnightPositions(xStart, yStart, n);
-		let numberOfKnights = 0;
-
-		let currentSquareList = this.adjacentList[`${xStart},${yStart}`];
-
-		let finalList = [];
-		let searchQueue = [];
-		currentSquareList.forEach(square => {
-			searchQueue.push(square);
-		});
-
-		// Cache for already checked squares
-		let checked = {};
-		checked[[xStart, yStart]] = 'checked';
-
-
-		while(searchQueue.length > 0) {
-			numberOfKnights++; // this is placed here to catch the initial push of the start list
-			let currentSquare = searchQueue.shift(); 
-			
-			// the first list does not make it to the rest of loop so has to be checked here
-			if ((currentSquare[0] == xEnd) && (currentSquare[1] == yEnd)) {
-				
-			// because Breadth-first search doubles the number of edges
-			// the final move count has to be divided by 2
-				return Math.round(numberOfKnights/2);
-			}
-
-			finalList.push(currentSquare);
-				if (this.adjacentList[`${currentSquare[0]},${currentSquare[1]}`] &&
-					!checked[`${currentSquare[0]},${currentSquare[1]}`]) {
-						this.adjacentList[`${currentSquare[0]},${currentSquare[1]}`].forEach(square => {
-							if(!checked[`${square[0]},${square[1]}`]) {
-								searchQueue.push(square);
-								checked[[currentSquare[0],currentSquare[1]]] = 'checked';
-                            }
-                        })
-                    }
-        }
-    }
+    return { pos, path }    // esle, return the posistion and path
 }
 
-const board = new knightBoard()
-const knightMoves = board.numberOfKnightMoves(2, 0, 0, 0, 5)
-console.log(knightMoves)
+function knightMoves([x, y], [a, b]) {
+    console.log([x, y])
+    let q = [Node([x, y], [[x, y]])]
+    console.log(q)
+    let curentNode = q.shift()
+
+    while (curentNode.pos[0] !== a || curentNode.pos[1] !== b) {
+        let moves = [
+            [curentNode.pos[0] + 2, curentNode.pos[1] - 1],
+            [curentNode.pos[0] + 2, curentNode.pos[1] + 1],
+            [curentNode.pos[0] - 2, curentNode.pos[1] - 1],
+            [curentNode.pos[0] - 2, curentNode.pos[1] + 1],
+            [curentNode.pos[0] + 1, curentNode.pos[1] - 2],
+            [curentNode.pos[0] + 1, curentNode.pos[1] + 2],
+            [curentNode.pos[0] - 1, curentNode.pos[1] - 2],
+            [curentNode.pos[0] - 1, curentNode.pos[1] + 2],
+        ]
+        moves.forEach((move) => {
+            let node = Node(move, curentNode.path.concat([move]))
+            if (node) {
+                q.push(node)
+            }
+        })
+        curentNode = q.shift()
+    }
+    console.log(
+        `=> You made it in ${curentNode.path.length - 1} moves! Here's your path:`
+    )
+    curentNode.path.forEach((pos) => {
+        console.log(pos)
+    })
+}
+
+knightMoves([3, 3], [4, 3])
